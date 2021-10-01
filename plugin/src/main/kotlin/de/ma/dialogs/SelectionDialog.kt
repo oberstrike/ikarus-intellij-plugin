c6fa9bbc53e671674691aee799fbf43a66cea878
oberstrike <markus.juergens@gmx.de>
2021-10-01 14:26:48 +0200

    Multimodule

--- plugin/src/main/kotlin/de/ma/dialogs/SelectionDialog.kt
+++ plugin/src/main/kotlin/de/ma/dialogs/SelectionDialog.kt
@@ -3,14 +3,11 @@ package de.ma.dialogs
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.DropdownMenu
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.awt.ComposePanel
-import com.google.wireless.android.sdk.stats.AppInspectionEvent
-import com.intellij.codeInspection.dataFlow.DataFlowInspectionBase
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import de.ma.dialogs.fragments.TextDropDown
